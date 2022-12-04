24e696bf0018c5254a02a73e105ba8cc4406b55a
idk <74161148+heywhyyoulookinghere@users.noreply.github.com>
2022-12-04 00:24:14 +0000

    Update vx.lua

--- vx.lua
+++ vx.lua
@@ -4,11 +4,11 @@ local chat = function(...)game.ReplicatedStorage.DefaultChatSystemChatEvents.Say
 local prefix = "-"
 
 function checkifplayer(input)
-  local function GetLongestName()
+	local function GetLongestName()
 		local tempGreatest = 0
-		
+
 		for i,v in pairs(game.Players:GetPlayers()) do
-			
+
 			if string.len(v.Name) > tempGreatest then
 				tempGreatest = string.len(v.Name)
 			end
@@ -16,24 +16,24 @@ function checkifplayer(input)
 				tempGreatest = string.len(v.DisplayName)
 			end
 		end
-		
+
 		return tempGreatest
 	end
-	
+
 	if string.len(input) ~= 0 then
 		if string.len(input) <= GetLongestName() then
 			local possibleplayers = {}
 			input = string.lower(input)
-			
+
 			for i,v in pairs(game.Players:GetPlayers()) do
 				local plrname = string.lower(v.Name)
 				local displayname = string.lower(v.DisplayName)
-				
+
 				if string.find(plrname, input) == 1 or string.find(displayname, input) == 1 then
 					table.insert(possibleplayers,v.Name)
 				end
 			end
-			
+
 			if #possibleplayers == 0 then
 				return "No player found."
 			elseif #possibleplayers == 1 then
@@ -48,21 +48,24 @@ function checkifplayer(input)
 end
 
 function sendnotif(msg)
-  game.StarterGui:SetCore("SendNotification", {
-      Title = "VX-V1";
-      Text = msg;
-      Duration = 3;
-      })
+	game.StarterGui:SetCore("SendNotification", {
+		Title = "VX-V1";
+		Text = msg;
+		Duration = 3;
+	})
 end
 
 game.Players.LocalPlayer.Chatted:Connect(function(chat)
-    local split = string.split(chat," ")
-    local lower = string.lower(split[1])
-    
-    if split[1] == prefix.."print" then
-      if game.Players:FindFirstChild(checkifplayer(split[2]) then
-        sendnotif(game.Players:FindFirstChild(checkifplayer[2].." is awesome")
-    end
+	local split = string.split(chat," ")
+	local lower = string.lower(split[1])
+
+	if lower == prefix.."print" then
+		if game.Players:FindFirstChild(checkifplayer(split[2])) then
+			sendnotif(checkifplayer(split[2]).." is awesome")
+		else
+			sendnotif(checkifplayer(split[2]))
+		end
+	end
 end)
 
 sendnotif("VX-V1 successfully loaded.")
